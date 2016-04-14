7644e87f9d3d1ac5897284937606ca056c67750e
Piotr Danowski <piter1403@gmail.com>
2016-04-14 18:13:34 +0200

    Info activity alpha

--- Pulsometer/Pulsometer/Activities/InfoActivity.cs
+++ Pulsometer/Pulsometer/Activities/InfoActivity.cs
@@ -8,19 +8,48 @@ using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Android.Util;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace Pulsometer.Activities
 {
-    [Activity(Label = "InfoActivity", Theme = "@style/MyTheme", ScreenOrientation = ScreenOrientation.Portrait)]
-    public class InfoActivity : Activity
+    [Activity(Label = "Informacje", Theme = "@style/MyTheme", ScreenOrientation = ScreenOrientation.Portrait)]
+    public class InfoActivity : AppCompatActivity
     {
+        private SupportToolbar toolbar;
+        private DrawerLayout mainDrawer;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Info);
+
+            InitializeObjects();
+            SetSupportActionBar(toolbar);
+            SetUpToolbar();
         }
+        private void SetUpToolbar()
+        {
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Finish(); 
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void InitializeObjects()
+        {
+            toolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            mainDrawer = FindViewById<DrawerLayout>(Resource.Id.drawerLayout);
+        }
+
     }
 }
